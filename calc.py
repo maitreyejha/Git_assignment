7f5b60378616f16f5365ca5b751423f1dd146d5b
maitreyejha <maitreyeejha4@gmail.com>
2021-10-14 11:52:58 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -2,3 +2,11 @@ def add(a,b):
 	return a+b
 def subtract(a,b):
 	return a-b
+def multiply(a,b):  
+	return a*bdef
+def divide(a,b):   
+	if(b == 0):       
+		return -1  
+	else :       
+		return a/b
+
